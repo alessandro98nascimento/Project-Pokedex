9dbb92c04638fbc94859ec097c54dc5bb7c4ac5f
Alessandro Santos do Nascimento <115187167+alessandro98nascimento@users.noreply.github.com>
2024-06-07 23:53:15 -0300

    começando a pag login front-end
    atribuindo card de login apenas css e elementos

--- Front-End/front-end/src/App.js
+++ Front-End/front-end/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className='background-img'>
+        <div className='div-card rounded'>
+          <div className='div-itens'>
+            <label for="input-Login">Login</label>
+            <input id="input-Login font-card" className='form-control' placeholder="Login"></input>
+
+            <label for="input-senha">Senha</label>
+            <input id="input-senha font-card" className='form-control' placeholder="Senha"></input>
+            <div className='div-btns'>
+              <button className="btn btn-primary padding-btns">Entrar</button>
+              <button className="btn btn-primary padding-btns">Cadastrar</button>
+            </div>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
